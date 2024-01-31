7820357f987add774b329bc60019d5b3e1dde6a4
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2024-01-31 22:34:18 +0000

    Deploying to gh-pages from @ Nearh777/hw-06-main@e5d8b510f392fe1c017e4f112f7f8e7bfd1176fd 🚀

---
+++ task-02.bd87b7e6.js
@@ -0,0 +1,2 @@
+document.querySelector("#ingredients").append(...["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((e=>{const t=document.createElement("Li");return t.textContent=e,t.classList.add("item"),t})));
+//# sourceMappingURL=task-02.bd87b7e6.js.map
