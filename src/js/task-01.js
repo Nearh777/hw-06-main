1654afe2864364abcfb78312661c81092b4ec653
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-01-31 20:55:39 +0000

    finish task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,7 @@
 const listEl = document.querySelectorAll('.item');
 
+console.log('Кількість категорій:', listEl.length);
+listEl.forEach(item => {
+  console.log('Категорія:', item.firstElementChild.textContent);
+  console.log('Кількість елементів:', item.lastElementChild.children.length);
+});
