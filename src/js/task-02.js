e5d8b510f392fe1c017e4f112f7f8e7bfd1176fd
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-01-31 22:33:18 +0000

    finish task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+ingredientsEl.append(
+  ...ingredients.map(ingredient => {
+    const itemEl = document.createElement('Li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+    return itemEl;
+  })
+);
