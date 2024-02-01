4b36c85dc3c38dcbe8d34deaeef59362af4baead
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2024-02-01 19:38:42 +0000

    Deploying to gh-pages from @ Nearh777/hw-06-main@778c128790849a3e2a25da3559fd36a5fafdec1e 🚀

---
+++ task-04.652a5a40.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("#counter");var e=document.querySelector("#value"),t=document.querySelector('[data-action="decrement"]'),n=document.querySelector('[data-action="increment"]'),c=0;e.textContent=c,t.addEventListener("click",(function(){c-=1,e.textContent=c})),n.addEventListener("click",(function(){c+=1,e.textContent=c}))}();
+//# sourceMappingURL=task-04.652a5a40.js.map
---
+++ task-04.835d93d8.js
@@ -0,0 +1,2 @@
+document.querySelector("#counter");const e=document.querySelector("#value"),t=document.querySelector('[data-action="decrement"]'),n=document.querySelector('[data-action="increment"]');let c=0;e.textContent=c,t.addEventListener("click",(function(){c-=1,e.textContent=c})),n.addEventListener("click",(function(){c+=1,e.textContent=c}));
+//# sourceMappingURL=task-04.835d93d8.js.map
