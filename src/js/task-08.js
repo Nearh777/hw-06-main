369ab59f3084248cc88c0320a8015182eaca70f4
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-02-02 11:29:54 +0000

    finish task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const registerFormEl = document.querySelector('.login-form');
 
+registerFormEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  }
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+  e.currentTarget.reset();
+}
