5f0506619c3424268572de9ebd4be22c98437dd2
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2024-02-02 10:32:57 +0000

    Deploying to gh-pages from @ Nearh777/hw-06-main@ec4916c1dd404ffcb3390095b3f101483fb663b4 🚀

---
+++ task-05.9fa6624a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#name-input"),e=document.querySelector("#name-output");t.addEventListener("input",(function(t){e.textContent=t.currentTarget.value.trim(),""===t.currentTarget.value&&(e.textContent="Anonymous")}));
+//# sourceMappingURL=task-05.9fa6624a.js.map
