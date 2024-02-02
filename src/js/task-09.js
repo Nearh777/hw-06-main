cf57a959399c69af723f7db48ff2f0727bd6d24d
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-02-02 11:39:25 +0000

    finish task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
