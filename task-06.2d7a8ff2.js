53314560f37a9b7083e64d5143840127e8857a13
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2024-02-02 10:52:45 +0000

    Deploying to gh-pages from @ Nearh777/hw-06-main@ad48877183118d2d6894e46bf1297e6434db3fc1 🚀

---
+++ task-06.2d7a8ff2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("#validation-input");t.addEventListener("blur",(function(a){a.currentTarget.value.length===Number(t.dataset.length)?t.classList.add("valid"):t.classList.add("invalid")}))}();
+//# sourceMappingURL=task-06.2d7a8ff2.js.map
---
+++ task-06.3355f0e4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#validation-input");t.addEventListener("blur",(function(e){e.currentTarget.value.length===Number(t.dataset.length)?t.classList.add("valid"):t.classList.add("invalid")}));
+//# sourceMappingURL=task-06.3355f0e4.js.map
