3759c60d64492f3f2e3fc42d15fbe42043b982a0
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-02-03 09:55:28 +0000

    finish task 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG');
+
+  const instance = basicLightbox.create(`
+  <img src="${e.target.dataset.source}" width="800" height="600">`);
+
+  instance.show();
+}
+
+const galleryItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}>
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', galleryItemsMarkup);
