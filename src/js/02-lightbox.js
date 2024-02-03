a3a273414b961971ef9308a28224655bade484a1
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-02-03 23:16:13 +0000

    finish 02-lightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+
+{
+  /* <li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li> */
+}
+
+const galleryItemsImgMarkup = ({ preview, original, description }) => {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      />
+      </a>
+      </li>`;
+};
+
+const makeGalleryItemsImg = galleryItems.map(galleryItemsImgMarkup).join('');
+
+galleryContainerEl.insertAdjacentHTML('beforeend', makeGalleryItemsImg);
+console.log(makeGalleryItemsImg);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
