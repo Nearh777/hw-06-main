d2aa3a7f1a6cf1387efed1d15194364562f34d6c
Сергей Трефель <56692519+Nearh777@users.noreply.github.com>
2024-02-03 20:16:50 +0000

    markup create

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,105 @@
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
-const galleryRef = document.querySelector('.gallery');
+const galleryContainerEl = document.querySelector('.gallery');
+// const itemsMarkup = galleryItemsMarkup(galleryItems);
+// galleryContainerEl.addEventListener('click', onGalleryClick);
 
-galleryRef.addEventListener('click', onGalleryClick);
+// function onGalleryClick(e) {
+//   e.preventDefault();
+
+//   if (e.target.nodeName !== 'IMG');
+
+//   const instance = basicLightbox.create(`
+//   <img src="${e.target.dataset.source}" width="800" height="600">`);
+
+//   instance.show();
+// }
+
+// function galleryItemsMarkup(items) {
+//   return items
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//     <a class="gallery__link" href="${original}>
+//     <img
+//     class="gallery__image"
+//     src="${preview}"
+//     data-source="${original}"
+//     alt="${description}"
+//     />
+//     </a>
+//     </div>`;
+//     })
+//     .join('');
+// }
+
+// galleryEl.insertAdjacentHTML('beforeend', itemsMarkup);
+
+galleryContainerEl.addEventListener('click', onGalleryClick);
+
+const galleryItemsMarkup = ({ preview, original, description }) => {
+  const itemImgEl = document.createElement('li');
+  itemImgEl.classList.add('gallery__item');
+
+  const linkEl = document.createElement('a');
+  linkEl.classList.add('gallery__link');
+  linkEl.setAttribute('href', `${original}`);
+
+  const imgEl = document.createElement('img');
+  imgEl.classList.add('gallery__image');
+  imgEl.setAttribute('src', `${preview}`);
+  imgEl.setAttribute('data-source', `${original}`);
+  imgEl.setAttribute('alt', `${description}`);
+
+  itemImgEl.append(linkEl, imgEl);
+
+  return itemImgEl;
+};
+
+const elementsImg = galleryItems.map(galleryItemsMarkup);
+
+galleryContainerEl.append(...elementsImg);
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: instance => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
 
 function onGalleryClick(e) {
   e.preventDefault();
 
-  if (e.target.nodeName !== 'IMG');
+  if (e.target.nodeName !== 'IMG') return;
 
-  const instance = basicLightbox.create(`
-  <img src="${e.target.dataset.source}" width="800" height="600">`);
+  const isItemImage = e.target.classList.contains('gallery__image');
+  if (!isItemImage) return;
 
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
   instance.show();
+
+  function onEscKeyPress(e) {
+    if (e.code !== 'Escape') return;
+    instance.close();
+  }
 }
-
-const galleryItemsMarkup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-    <a class="gallery__link" href="${original}>
-    <img
-    class="gallery__image"
-    src="${preview}"
-    data-source="${original}"
-    alt="${description}"
-    />
-    </a>
-    </div>`;
-  })
-  .join('');
-
-galleryRef.insertAdjacentHTML('beforeend', galleryItemsMarkup);
